9ed2165ade37ffcdecc7a3eb362131b30760c370
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-03-06 15:19:53 +0200

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer
+{
+    class Customer
+    {
+        public int ID
+        {
+            get { return this.ID}
+            set { this.ID}
+        }
+        public string Name { get; set; }
+        public int BirthYEAR { get; set; }
+        public string Address { get; set; }
+        public int Protection { get; set; }
+        public int TotalPurchase { get; set; }
+        public MyQueue(int id, string name, int birthyear, string address, int protection, int totalpurchase)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.BirthYEAR = birthyear;
+            this.Address = address;
+            this.Protection = protection;
+            this.TotalPurchase = totalpurchase;
+
+        }
+
+        public override string ToString()
+        {
+            return $"id: {ID} name: {Name} birthyear: {BirthYEAR} address: {Address} protection: {Protection} totalpurchase {TotalPurchase}"
+        }
+    }
+    class SortByProtection : IComparer<Customer>
+    {
+        public int Compare(Customer x, Customer y)
+        {
+            return x.Protection - y.Protection;
+        }
+    }
+    class SortByTotalPurchase : IComparer<Customer>
+    {
+        public int Compare(Customer x, Customer y)
+        {
+            return x.TotalPurchase - y.TotalPurchase;
+        }
+    }
+    class SortByBirthYear : IComparer<Customer>
+    {
+        public int Compare(Customer x, Customer y)
+        {   
+            return -1*(x.TotalPurchase - y.TotalPurchase);
+        }
+    }
+}
---
+++ MyQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer
+{
+    class MyQueue
+    {
+        private List<Customer> customers = new List<Customer>();
+        public void Enqueue (Customer clinet)
+        {
+            customers.Insert(0, clinet);
+        }
+        public Customer Dequeue()
+        {
+            if(customers.Count > 0)
+            {
+                Customer result = customers[0];
+                customers.RemoveAt(0);
+                return result;
+            }
+            return null;      
+        }
+        public void InIt(List<Customer> x)
+        {
+            this.customers = x;
+        }
+        public void Clear()
+        {
+            customers.Clear();
+        }
+        public Customer WholsNext()
+        {           
+            return customers[0];
+        }
+        public int Count
+        {
+            get
+            {
+                return customers.Count;
+            }
+        }
+        public void SortByProtecion()
+        {
+            SortByProtecion();         
+        }
+        public void SortByTotalPurchase()
+        {
+            SortByTotalPurchase();
+        }
+        public void SortByBirthYear()
+        {
+            SortByBirthYear();
+        }
+        public List<Customer> DequeueCustomers(int x)
+        {
+            return customers.Removeall(x, 0-4);
+        }
+        public void AniRakSheela(Customer x)
+        {
+
+        }
+        public Customer DequeueProtectiza()
+        {
+            Customer result = customers[1];
+        }
+    }
+}
